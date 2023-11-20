587e2dbf1f4c9690be669828c2d0fdfdd860e750
Lucas Lima <lucasViniLima15@gmail.com>
2023-11-20 20:12:56 -0300

    adicionando css e tasklist component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { TaskListComponent } from './task-list/task-list.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
