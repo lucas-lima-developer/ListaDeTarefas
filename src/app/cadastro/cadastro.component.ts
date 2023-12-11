8b1bdf8b6f1d270a2806c4dc1df58c166d91ed91
Lucas Lima <lucasViniLima15@gmail.com>
2023-12-10 22:10:15 -0300

    componente de cadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+  cadastroForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
 
+  errorMessage = '';
+
+  constructor(private UserService: UserService) {}
+
+  submitCadastroForm() {
+    this.UserService.cadastrar(this.cadastroForm.value.email ?? '', this.cadastroForm.value.password ?? '');
+  }
 }
